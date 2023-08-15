92a397d360a9e488bcb62ddffc55ca6a5d909917
atroopa <atroopa@gmail.com>
2023-08-15 13:02:38 -0700

    i happy make firs go blockchain code i read a block number

---
+++ 1_start/client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+)
+
+var unfuraURL = "https://mainnet.infura.io/v3/f67125134e064cf094e2495c49323c68"
+
+func main() {
+	client, err := ethclient.DialContext(context.Background(), unfuraURL)
+
+	if err != nil {
+		log.Fatalf("Error: error to create a ether client -> %v", err)
+		return
+	}
+
+	defer client.Close()
+
+	block, err := client.BlockByNumber(context.Background(), nil)
+
+	if err != nil {
+		log.Fatalf("Error: error to get Block -> %v", err)
+		return
+	}
+
+	fmt.Println(block.Number())
+}
