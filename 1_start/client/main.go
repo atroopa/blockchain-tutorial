60ff93ac3348ed9e8296bc0d6014bd45d5b8e4f5
atroopa <atroopa@gmail.com>
2023-08-16 14:16:28 -0700

    add ganache to url

--- 1_start/client/main.go
+++ 1_start/client/main.go
@@ -8,9 +8,10 @@ import (
 )
 
 var unfuraURL = "https://mainnet.infura.io/v3/f67125134e064cf094e2495c49323c68"
+var ganacheURL = "http://127.0.0.1:7545"
 
 func main() {
-	client, err := ethclient.DialContext(context.Background(), unfuraURL)
+	client, err := ethclient.DialContext(context.Background(), ganacheURL)
 
 	if err != nil {
 		log.Fatalf("Error: error to create a ether client -> %v", err)
